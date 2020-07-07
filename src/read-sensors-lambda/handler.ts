5c60b65ec8da98a0ce8479ce9862726289fc6b21
Chris Lyth <lythchr@amazon.com>
2020-07-06 23:16:22 -0700

    removed the serial read attempt

--- src/read-sensors-lambda/handler.ts
+++ src/read-sensors-lambda/handler.ts
@@ -14,36 +14,7 @@ const STREAM_NAME = 'USB0';
 
 
 function greengrassHandler(){
-    const c = new StreamManagerClient();
-    c.onConnected(async () => {
-        // Try deleting the stream (if it exists) so that we have a fresh start
-        try {
-            await c.deleteMessageStream(STREAM_NAME);
-        } catch (e) {
-            // Rethrow the error if it wasn't the expected error
-            if (!(e instanceof ResourceNotFoundException)) {
-                throw e;
-            }
-        }
-    
-        try {
-    
-            await c.createMessageStream(
-                new MessageStreamDefinition()
-                    .withName(STREAM_NAME)
-                    .withStrategyOnFull(StrategyOnFull.OverwriteOldestData),
-            );
-    
-            // Try reading the 2 messages we just appended and print them out
-            console.log(`Successfully read 2 messages: ${
-                await c.readMessages(STREAM_NAME,
-                    new ReadMessagesOptions()
-                        .withMinMessageCount(2)
-                        .withReadTimeoutMillis(1000))}`);
-        } catch {
-            c.close();
-        }
-    });
+    console.log('TODO: implement the sensor read in JS');
 }
 
 
